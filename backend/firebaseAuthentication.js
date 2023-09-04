594d697348cc2895643227d7ac133b29cceca69b
paulr10463 <paul.roman@epn.edu.ec>
2023-09-04 10:40:21 -0500

    feat: npm build in backend

--- backend/firebaseAuthentication.js
+++ backend/firebaseAuthentication.js
@@ -1,33 +1,32 @@
-const admin = require('firebase-admin');
-require('dotenv').config();
+const admin = require('firebase-admin')
+require('dotenv').config()
 
 const firebaseConfig = {
-    projectId: process.env.PROJECT_ID,
-    privateKeyId: process.env.PRIVATE_KEY_ID,
-    privateKey: process.env.PRIVATE_KEY,
-    clientEmail: process.env.CLIENT_EMAIL,
-    clientId: process.env.CLIENT_ID,
-    authUri: process.env.AUTH_URI,
-    tokenUri: process.env.TOKEN_URI,
-    authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    clientX509CertUrl: process.env.CLIENT_X509_CERT_URL,
-    universeDomain: process.env.UNIVERSE_DOMAIN
-  };
+  privateKeyId: process.env.PRIVATE_KEY_ID,
+  privateKey: process.env.PRIVATE_KEY,
+  clientEmail: process.env.CLIENT_EMAIL,
+  clientId: process.env.CLIENT_ID,
+  authUri: process.env.AUTH_URI,
+  tokenUri: process.env.TOKEN_URI,
+  authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  clientX509CertUrl: process.env.CLIENT_X509_CERT_URL,
+  universeDomain: process.env.UNIVERSE_DOMAIN,
+  projectId: process.env.PROJECT_ID
+}
 
 // Inicializa Firebase Admin SDK con tu configuración
-let adminApp = admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
-  // Opcional: También puedes especificar tu configuración de Firebase aquí
-});
+const adminApp = admin.initializeApp({
+  credential: admin.credential.cert(firebaseConfig)
+})
 
-console.log('Firebase Admin SDK inicializado', adminApp.name);
+console.log('Firebase Admin SDK inicializado', adminApp.name)
 
 // Middleware de autenticación
-function isAuthenticated(req, res, next) {
-  const auth = admin.auth();
+function isAuthenticated (req, res, next) {
+  const auth = admin.auth()
   // Verifica si el usuario está autenticado
-  const idToken = req.header('Authorization');
-    console.log("En firebase Authentication"+idToken);
+  const idToken = req.header('Authorization')
+  console.log('En firebase Authentication' + idToken)
   if (!idToken) {
     return res.status(401).json({ error: 'Acceso no autorizado - Token no proporcionado' });
   }
@@ -36,14 +35,14 @@ function isAuthenticated(req, res, next) {
     .verifyIdToken(idToken)
     .then((decodedToken) => {
       // El token es válido, puedes acceder a la información del usuario en decodedToken
-      req.user = decodedToken;
-      console.log('Token válido, usuario autenticado', decodedToken);
-      next();
+      req.user = decodedToken
+      console.log('Token válido, usuario autenticado', decodedToken)
+      next()
     })
     .catch((error) => {
-      console.error('Error de verificación de token:', error);
-      return res.status(401).json({ error: 'Acceso no autorizado - Token inválido' });
-    });
+      console.error('Error de verificación de token:', error)
+      return res.status(401).json({ error: 'Acceso no autorizado - Token inválido' })
+    })
 }
 
-module.exports = isAuthenticated, adminApp;
+module.exports = isAuthenticated
--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-};
+  appId: process.env.APP_ID
+}
 
-module.exports = firebaseConfig;
+module.exports = firebaseConfig
