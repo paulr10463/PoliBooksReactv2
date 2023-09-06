b8c25be6f98f6028ecc900c173d1d1ad042f245e
gabriela-mn <cris_1997cm@hotmail.com>
2023-09-06 11:37:54 -0500

    feat: Comment

--- backend/firebaseAuthentication.js
+++ backend/firebaseAuthentication.js
@@ -1,6 +1,7 @@
 const admin = require('firebase-admin')
 require('dotenv').config()
 
+// Se configura la información de autenticación de Firebase desde variables de entorno
 const firebaseConfig = {
   privateKeyId: process.env.PRIVATE_KEY_ID,
   privateKey: process.env.PRIVATE_KEY,
@@ -14,7 +15,7 @@ const firebaseConfig = {
   projectId: process.env.PROJECT_ID
 }
 
-// Inicializa Firebase Admin SDK con tu configuración
+// Inicializa Firebase Admin SDK con la configuración proporcionada
 const adminApp = admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig)
 })
@@ -24,10 +25,11 @@ console.log('Firebase Admin SDK inicializado', adminApp.name)
 // Middleware de autenticación
 function isAuthenticated (req, res, next) {
   const auth = admin.auth()
-  // Verifica si el usuario está autenticado
+  // Verifica si el usuario está autenticado utilizando el token proporcionado en el encabezado de autorización
   const idToken = req.header('Authorization')
   console.log('En firebase Authentication' + idToken)
   if (!idToken) {
+    // Si no se proporciona un token en el encabezado, se responde con un error de acceso no autorizado
     return res.status(401).json({ error: 'Acceso no autorizado - Token no proporcionado' });
   }
 
@@ -44,5 +46,5 @@ function isAuthenticated (req, res, next) {
       return res.status(401).json({ error: 'Acceso no autorizado - Token inválido' })
     })
 }
-
+// Exporta el middleware de autenticación
 module.exports = isAuthenticated
