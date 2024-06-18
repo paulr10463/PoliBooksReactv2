84fddf650f8c4878ec96d1df1b5c3216b8e55973
paulr10463 <paul.roman@epn.edu.ec>
2024-06-18 12:53:58 -0500

    feat: websockets working

---
+++ frontend/src/services/websocket.service.js
@@ -0,0 +1,29 @@
+
+const socket = new WebSocket('ws://localhost:5000');
+
+const chatMessages = [];
+
+socket.addEventListener('open', (event) => {
+    console.log('Connection established with the WebSocket server');
+    socket.send('Hello, server!');
+});
+
+//socket.onmessage = (event) => { };
+socket.addEventListener('message', (event) => {
+    event.data.arrayBuffer().then((data) => {
+        const message = new TextDecoder('utf-8').decode(data);
+        //console.log(message); 
+        chatMessages.push(message);
+    });
+}); 
+
+
+function sendMessage(message) {
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(message);
+    }
+}
+
+function getMessages() {
+    return chatMessages;
+}
