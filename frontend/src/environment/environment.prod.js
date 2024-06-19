809eb0ff55c315f7bf7df9fc248e52949ea9c56c
paulr10463 <paul.roman@epn.edu.ec>
2024-06-19 10:11:09 -0500

    Initial commit

--- frontend/src/environment/environment.prod.js
+++ frontend/src/environment/environment.prod.js
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    HOST: 'https://polibooksapi.azurewebsites.net/api',
-    //HOST: 'http://localhost:3000/api',
+    //HOST: 'https://polibooksapi.azurewebsites.net/api',
+    HOST: 'http://localhost:3000/api',
     WS: "ws://localhost:3000"
 };
