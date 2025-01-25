47c93c678156b9633d3a0ed86470f3ae81f84da6
paulr10463 <paul.roman@epn.edu.ec>
2025-01-24 22:22:18 -0500

    feat: Commit help

--- frontend/src/environment/environment.prod.js
+++ frontend/src/environment/environment.prod.js
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     //HOST: 'https://polibooksapi.azurewebsites.net/api',
     HOST: 'https://polibooksreactv2.onrender.com/api',
-    WS: "ws://18.119.163.240/"
+    WS: "ws://18.119.163.240/" 
 };
